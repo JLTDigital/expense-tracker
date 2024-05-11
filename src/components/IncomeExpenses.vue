e905a89779a8fa463daef410bc324d483ecec633
JLTDigital <mrjltaft@gmail.com>
2024-05-11 19:27:16 +0100

    init

---
+++ src/components/IncomeExpenses.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="inc-exp-container">
+    <div>
+      <h4>Income</h4>
+      <div id="money-plus" class="money plus">+£0.00</div>
+    </div>
+    <div>
+      <h4>Expenses</h4>
+      <div id="money-minus" class="money minus">-£0.00</div>
+    </div>
+  </div>
+</template>

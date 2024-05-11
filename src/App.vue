c02940d937aa0a4ef03676a6453c065469fed78a
JLTDigital <mrjltaft@gmail.com>
2024-05-11 19:53:34 +0100

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
   <Header />
   <div class="container">
-    <Balance />
+    <Balance v-bind:total="total" />
     <IncomeExpenses />
-    <TransactionList />
+    <TransactionList v-bind:transactions="transactions" />
+    <AddTransaction />
   </div>
 </template>
 
-<script>
+<script setup>
 import Header from './components/Header.vue'
 import Balance from './components/Balance.vue'
 import IncomeExpenses from './components/IncomeExpenses.vue'
 import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
 
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpenses,
-    TransactionList,
-  },
-}
+import { ref, computed } from 'vue'
+
+const transactions = ref([
+  { id: 1, text: 'FLower', amount: -19.99 },
+  { id: 2, text: 'Salary', amount: 219.97 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 },
+])
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount
+  }, 0)
+})
 </script>
-
-<style scoped></style>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,57 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">
-      Cash <span>-$400</span><button class="delete-btn">x</button>
-    </li>
-    <li class="plus">
-      Pay Check <span>+$600</span><button class="delete-btn">x</button>
+    <li
+      v-for="transaction in transactions"
+      v-bind:key="transaction.id"
+      v-bind:class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>£{{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
     </li>
   </ul>
 </template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
+
+<!-- Options API Example below -->
+<!-- <script>
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: 'FLower', amount: -19.99 },
+        { id: 2, text: 'Salary', amount: 219.97 },
+        { id: 3, text: 'Book', amount: -10 },
+        { id: 4, text: 'Camera', amount: 150 },
+      ],
+    }
+  },
+}
+</script> -->
+
+<!-- Composition API Example with explicit setup() function -->
+<!-- <script>
+export default {
+  setup() {
+    const transactions = [
+      { id: 1, text: 'FLower', amount: -19.99 },
+      { id: 2, text: 'Salary', amount: 219.97 },
+      { id: 3, text: 'Book', amount: -10 },
+      { id: 4, text: 'Camera', amount: 150 },
+    ]
+    return {
+      transactions,
+    }
+  },
+}
+</script> -->

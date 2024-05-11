90107043e0203fff0281e191512feeed57e2b008
JLTDigital <mrjltaft@gmail.com>
2024-05-11 20:38:34 +0100

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <Header />
   <div class="container">
-    <Balance v-bind:total="total" />
-    <IncomeExpenses />
-    <TransactionList v-bind:transactions="transactions" />
-    <AddTransaction />
+    <Balance v-bind:total="+total" />
+    <IncomeExpenses v-bind:income="+income" v-bind:expenses="+expenses" />
+    <TransactionList
+      v-bind:transactions="transactions"
+      @transactionDeleted="handleTransactionDeleted"
+    />
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted" />
   </div>
 </template>
 
@@ -14,19 +17,79 @@ import Balance from './components/Balance.vue'
 import IncomeExpenses from './components/IncomeExpenses.vue'
 import TransactionList from './components/TransactionList.vue'
 import AddTransaction from './components/AddTransaction.vue'
+import { useToast } from 'vue-toastification'
 
-import { ref, computed } from 'vue'
+const toast = useToast()
 
-const transactions = ref([
-  { id: 1, text: 'FLower', amount: -19.99 },
-  { id: 2, text: 'Salary', amount: 219.97 },
-  { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: 150 },
-])
+import { ref, computed, onMounted } from 'vue'
 
+const transactions = ref([])
+
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem('transactions'))
+  if (savedTransactions) {
+    transactions.value = savedTransactions
+  }
+})
+
+// Get Total
 const total = computed(() => {
   return transactions.value.reduce((acc, transaction) => {
     return acc + transaction.amount
   }, 0)
 })
+
+// Get Income
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount
+    }, 0)
+    .toFixed(2)
+})
+
+// Get Expenses
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount
+    }, 0)
+    .toFixed(2)
+})
+
+// Add transaction
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount,
+  })
+
+  saveToLocalStorage()
+
+  toast.success('Transaction Added')
+}
+
+// Generate ID
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1000000)
+}
+
+// Delete Transaction
+const handleTransactionDeleted = (id) => {
+  transactions.value = transactions.value.filter(
+    (transaction) => transaction.id !== id
+  )
+
+  handleTransactionDeleted()
+
+  toast.success('Transaction Deleted')
+}
+
+// Save to Local Storage
+const saveToLocalStorage = () => {
+  localStorage.setItem('transactions', JSON.stringify(transactions.value))
+}
 </script>
--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -2,11 +2,26 @@
   <div class="inc-exp-container">
     <div>
       <h4>Income</h4>
-      <div id="money-plus" class="money plus">+£0.00</div>
+      <div id="money-plus" class="money plus">+£{{ income }}</div>
     </div>
     <div>
       <h4>Expenses</h4>
-      <div id="money-minus" class="money minus">-£0.00</div>
+      <div id="money-minus" class="money minus">£{{ expenses }}</div>
     </div>
   </div>
 </template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  income: {
+    type: Number,
+    required: true,
+  },
+  expenses: {
+    type: Number,
+    required: true,
+  },
+})
+</script>
